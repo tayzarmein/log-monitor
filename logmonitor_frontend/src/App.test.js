29e35a4739b248c4a03b6a7b77041d9bc79a0e6a
Win Tayzar Mein <tayzarmein@gmail.com>
2020-07-12 18:14:04 +0900

    implemented oldgen graph.

--- logmonitor_frontend/src/App.test.js
+++ logmonitor_frontend/src/App.test.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+/**
+ * @jest-environment node
+ */
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import Axios from 'axios';
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('my_test async', () => {
+  return Axios.get('http://127.0.0.1:8000/api/', {
+    params: {
+      type: 'oldgen',
+    }
+  })
+    .then(data => {
+      console.log("data=", data.data);
+      expect(data.data instanceof Array).toBe(true)
+    })
+})
